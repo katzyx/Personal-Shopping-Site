9b4f9b98e913022490f8fa9bec84ef525b1b2e97
Floria Fang Zhang <floria.fang9@gmail.com>
2024-05-11 16:20:23 -0400

    Creation of Product class

---
+++ product_selection/__init__.py
@@ -0,0 +1,25 @@
+'''
+Description:
+Authors: Floria Fang Zhang, Hope Hadfield, Katherine Zhang, Maisey Perelonia
+'''
+
+# Imports
+from pydantic import BaseModel
+
+# Class Definitions
+class Product(BaseModel):
+    name: str # Product name
+    type: str # Product type
+    color: list[str] # List of all product colors (all shades)
+    price: float # Price of product in CAD
+    size: dict = dict.fromkeys(['oz', 'g', 'mL'], None) # Product size in ounces, grams, mililleters
+    formula: str # Product formula
+    ingredients: list[str] # List of ingredients of product
+    about: str # Description of product
+    url: str # URL to product purchasing site
+
+
+
+
+
+
