947871a1eb2c0a6e9c92ce46ee73499a41e58b2d
Katherine Zhang <katherinezhang02@gmail.com>
2024-05-11 16:41:39 -0400

    init

--- product_selection/select_product.py
+++ product_selection/select_product.py
@@ -1,27 +1,8 @@
-'''
-Description:
-Authors: Floria Fang Zhang, Hope Hadfield, Katherine Zhang, Maisey Perelonia
-'''
-
-# Imports
-from pydantic import BaseModel
+import product_selection
 import pandas
 import csv
 
-# Class Definitions
-class Product(BaseModel):
-    name: str # Product name
-    type: str # Product type
-    color: list[str] # List of all product colors (all shades)
-    price: float # Price of product in CAD
-    size: dict = dict.fromkeys(['oz', 'g', 'mL'], None) # Product size in ounces, grams, mililleters
-    formula: str # Product formula
-    ingredients: list[str] # List of ingredients of product
-    about: str # Description of product
-    url: str # URL to product purchasing site
+def parse_dataset():
+    
 
-
-
-
-
-
+    return
