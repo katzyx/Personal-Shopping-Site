1317a43789db8ff5119af5b661978e8e069313f8
Floria Fang Zhang <floria.fang9@gmail.com>
2024-05-11 16:58:42 -0400

    Implementing code to parse data from CSV into product database

--- product_selection/__init__.py
+++ product_selection/__init__.py
@@ -11,6 +11,7 @@ from pydantic import BaseModel
 class Product(BaseModel):
     name: str # Product name
     type: str # Product type
+    brand: str # Product brand
     color: list[str] # List of all product colors (all shades)
     price: float # Price of product in CAD
     size: dict = dict.fromkeys(['oz', 'g', 'mL'], None) # Product size in ounces, grams, mililleters
@@ -26,8 +27,26 @@ class BasicSelection(BaseModel):
     def parse_dataset(self):
         df = pd.read_csv(self.csv_file)
         for entry in df.rows:
+            # Create product object to append to product database
             temp_product = Product(name = df.entry['Name'], 
-                                   type = df.entry['Product Type'])
+                                   type = df.entry['Product Type'],
+                                   brand = df.entry['Brand'],
+                                   color = df.entry['Color'],
+                                   price = df.entry['Price'],
+                                   formula = df.entry['Formula'],
+                                   ingredients = df.entry['Ingredients'],
+                                   about = df.entry['About'],
+                                   url = df.entry['URL'])
+            
+            # Extract size of product
+            size = df.entry['Size']
+            size = size.split('/')
+            for measure in size:
+                measure = measure.replace(' ', '')
+                for key in list(temp_product.size.keys()):
+                    if measure.endswith(key):
+                        temp_product.size[key] = int(measure.split(key)[0])
+            
             self.product_database.append(temp_product)
     
 
