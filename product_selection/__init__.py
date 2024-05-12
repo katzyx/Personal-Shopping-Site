e2ddafa1822f560fd0d30fc822b8da927d50655e
unknown <katherinezhang02@gmail.com>
2024-05-11 23:54:40 -0400

    format data

--- product_selection/__init__.py
+++ product_selection/__init__.py
@@ -15,7 +15,7 @@ class Product(BaseModel):
     brand: str # Product brand
     color: list[str] # List of all product colors (all shades)
     price: float # Price of product in CAD
-    size: dict = dict.fromkeys(['oz', 'g', 'mL'], None) # Product size in ounces, grams, mililleters
+    size: str # Product size in metric given
     formula: str # Product formula
     ingredients: list[str] # List of ingredients of product
     about: str # Description of product
@@ -24,25 +24,36 @@ class Product(BaseModel):
 class BasicSelection(BaseModel):
     csv_file: str # Link to csv file
     # product_databse: list[Product] # list of products
-    
+
     def parse_dataset(self):
         df = pd.read_csv(self.csv_file)
-        print("data set: ", self.csv_file)
         print("rows: ", df.shape[0])
         print("columns: ", df.shape[1])
 
         for r in range(1, df.shape[0]):
+            
+
             # Create product object to append to product database
+
+            # split string from dataset into list
+            color = df.iloc[r, 2]
+            color_list = color.split(',')
+
+            ingr = df.iloc[r, 7]
+            ingr_list = ingr.split(',')
+
             temp_product = Product(name = df.iloc[r, 0],
                                    type = df.iloc[r, 1],
                                    brand = df.iloc[r, 2],
-                                   color = df.iloc[r, 3],
+                                   color = color_list,
                                    price = df.iloc[r, 4],
-                                   size = df.iloc[r, 5],
+                                   size = str(df.iloc[r, 5]),
                                    formula = df.iloc[r, 6],
-                                   ingredients = df.iloc[r, 7],
+                                   ingredients = ingr_list,
                                    about = df.iloc[r, 8],
                                    url = df.iloc[r, 9]) 
+        
+        print("dataframe: ", df)
             
             # Extract size of product
             # size = df.entry['Size']
