daf87129b18b9c0b374021235bf06a48ef8da804
Floria Fang Zhang <floria.fang9@gmail.com>
2024-09-26 15:17:07 -0400

    Implementing search bar for index page.

--- shopping_site.py
+++ shopping_site.py
@@ -13,23 +13,36 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/index', methods=['GET', 'POST'])
-def index():
+# Rendering Landing Page
+@app.route('/', methods=['GET', 'POST'])
+def landing_page():
     if request.method == 'POST':
         # Get raw 'who' and 'what' input
         user_details = request.form.get('user_details')
         product_preferences = request.form.get('product_preferences')
-
-        # Call Python function to map inputs to products
-        products_list: list[Product] = []
-        products_list = map_inputs(user_details,product_preferences)
-
-
-        return render_template('index.html', products_list=products_list)
+        
+        # Redirecting to index
+        return redirect(url_for('index', user_details=user_details, product_preferences=product_preferences))
+    
+    # Rendering landing page
     return render_template('landing_page.html')
 
+# Rending Products Page
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get updated 'what' input and re-render page
+        updated_preferences = request.form.get('updated_preferences')
+        return redirect(url_for('index', product_preferences=updated_preferences))
+    
+    # Get who and what from landing page
+    user_details = request.args.get('user_details', '')
+    product_preferences = request.args.get('product_preferences', '')
 
-@app.route('/', methods=['GET', 'POST'])
-def landing_page():
-    return render_template('landing_page.html')
+    # Call Python function to map inputs to products
+    products_list: list[Product] = map_inputs(user_details,product_preferences)
 
+    return render_template('index.html', products_list=products_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
