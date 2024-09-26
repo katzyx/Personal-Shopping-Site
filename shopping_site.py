c1068905f254acf117265817dfeafedbb37dff97
Floria Fang Zhang <floria.fang9@gmail.com>
2024-09-26 15:29:48 -0400

    Make who and what persistent

--- shopping_site.py
+++ shopping_site.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.secret_key = 'hello'
+
 # Rendering Landing Page
 @app.route('/', methods=['GET', 'POST'])
 def landing_page():
@@ -20,9 +22,13 @@ def landing_page():
         # Get raw 'who' and 'what' input
         user_details = request.form.get('user_details')
         product_preferences = request.form.get('product_preferences')
+
+        # Store user_details and product_preferences
+        session['user_details'] = user_details
+        session['product_preferences'] = product_preferences
         
         # Redirecting to index
-        return redirect(url_for('index', user_details=user_details, product_preferences=product_preferences))
+        return redirect(url_for('index'))
     
     # Rendering landing page
     return render_template('landing_page.html')
@@ -30,17 +36,19 @@ def landing_page():
 # Rending Products Page
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    # Get who and what from landing page
+    user_details = session.get('user_details')
+    product_preferences = session.get('product_preferences')
+
     if request.method == 'POST':
         # Get updated 'what' input and re-render page
         updated_preferences = request.form.get('updated_preferences')
-        return redirect(url_for('index', product_preferences=updated_preferences))
-    
-    # Get who and what from landing page
-    user_details = request.args.get('user_details', '')
-    product_preferences = request.args.get('product_preferences', '')
+        session['product_preferences'] = updated_preferences
+
+        return redirect(url_for('index'))
 
     # Call Python function to map inputs to products
-    products_list: list[Product] = map_inputs(user_details,product_preferences)
+    products_list: list[Product] = map_inputs(user_details, product_preferences)
 
     return render_template('index.html', products_list=products_list)
 
