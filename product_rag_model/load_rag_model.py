8eeb80999e15eaabf050e574f59a464162b72f83
Floria Fang Zhang <floria.fang9@gmail.com>
2025-03-16 16:44:31 -0400

    Connect RAG model to website. Upgrade Blogpost and UserInput OpenAI

--- product_rag_model/load_rag_model.py
+++ product_rag_model/load_rag_model.py
@@ -2,6 +2,7 @@ import os
 import glob
 import chromadb # type: ignore
 import time
+import json
 from llama_index.core import VectorStoreIndex, StorageContext, get_response_synthesizer # type: ignore
 from llama_index.core.readers.json import JSONReader # type: ignore
 from llama_index.llms.openai import OpenAI # type: ignore
@@ -10,6 +11,18 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding # type: igno
 from llama_index.core import Settings # type: ignore
 from llama_index.core.retrievers import VectorIndexRetriever # type: ignore
 from llama_index.core.query_engine import RetrieverQueryEngine # type: ignore
+
+# Class Definitions
+class RAGProduct:
+    def __init__(self, name, brand, price, url, redirect_url):
+        self.name: str = name # Product name
+        self.brand: str = brand # Product brand
+        self.price: float = price # Price of product in CAD
+        self.url: str = url # URL to product image
+        self.redirect_url: str = redirect_url # URL to purchasing site
+    
+    def __str__(self) -> str:
+        return f"Product Name: {self.name}\nBrand: {self.brand}\nPrice: {self.price}\nImage URL: {self.url}\nProduct URL: {self.redirect_url}\n\n"
 
 # Function to load and index JSON data
 def load_index(index_dir):
@@ -37,6 +50,18 @@ def load_index(index_dir):
     
     return None
 
+
+def format_products(response):
+    json_data = json.loads(str(response))
+    products_list: list[RAGProduct] = []
+
+    for product in json_data["products"]:
+        new_product = RAGProduct(product["product_name"], product["brand"], product["price"].replace('$',''), product["image_url"], product["product_url"])
+        products_list.append(new_product)
+    
+    return products_list
+
+
 def query_rag_model(index, who_input, what_input):
     if not index:
         print("Index not found.")
@@ -60,29 +85,29 @@ def query_rag_model(index, who_input, what_input):
     )
     
     query_text = f"\
-Who I am: {who_input}. What I want: {what_input}. Recommend me 11 (eleven) products. Return each product in json format \
-(under key 'products') with the following keys: 'product_id', 'product_name', 'brand', 'product_url', 'image_url'"
+Who I am: {who_input}. What I want: {what_input}. Recommend the 15 (fifteen) most relevant products. Return each product in json format \
+(under key 'products') with the following keys: 'product_id', 'product_name', 'brand', 'price', 'product_url', 'image_url'. "
 # For each product, if Shades are available, choose one shade for me whose name and description matches best with my features and wants. \
 # Then, add the keys 'shade_name', 'shade_description', 'shade_image_url' to the product json IF AND ONLY IF a shade is found (otherwise DO NOT ADD THESE KEYS)."
-    print(query_text)
 
     response = query_engine.query(query_text)
-    print(response)
+    # print(query_text, response)
 
-    return response
-
-
+    return format_products(response)
 
 if __name__ == "__main__": 
     start = time.time()
 
+    # index_dir = "./chroma_db"
     index_dir = "./chroma_db"
     index = load_index(index_dir)
 
     loaded_index = time.time()
 
-    query_rag_model(index, "I am a 23 year old Asian woman with a dark-deep neutral skintone.", "I'm looking for a full face of makeup for a party.")
-
+    result = query_rag_model(index, "I am a 23 year old Asian woman with a dark-deep neutral skintone.", "I'm looking for a full face of makeup for a party.")
+    for product in result:
+        print(product)
+    
     queried = time.time()
     print(f"\nTime to load index: {loaded_index - start} seconds.")
     print(f"Time to generate response: {queried - loaded_index} seconds.")
